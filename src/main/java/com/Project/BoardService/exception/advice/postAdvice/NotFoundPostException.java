f4f6e63b86b775f93054ce56b6cd6835c02316cf
kkangh00n <kig2454@gmail.com>
2023-06-21 00:21:10 +0900

    fix: 패키지 & 명령어 변경

--- src/main/java/com/Project/BoardService/exception/advice/postAdvice/NotFoundPostException.java
+++ src/main/java/com/Project/BoardService/exception/advice/postAdvice/NotFoundPostException.java
@@ -1,9 +1,9 @@
-package com.Project.BoardService.exception.postAdvice;
+package com.Project.BoardService.exception.advice.postAdvice;
 
 import com.Project.BoardService.exception.advice.NotFoundException;
 
 public class NotFoundPostException extends NotFoundException {
-    private final static String MESSAGE = "존재하지 않는 게시물입니다";
+    private static final String MESSAGE = "존재하지 않는 게시물입니다";
     public NotFoundPostException() {
         super(MESSAGE);
     }
