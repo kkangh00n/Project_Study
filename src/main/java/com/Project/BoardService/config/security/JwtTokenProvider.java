0097261ca8eb66b39d2cd7612c10e668214a1561
kkangh00n <kig2454@gmail.com>
2023-06-27 19:17:33 +0900

    feat: JWT 토큰 생성

---
+++ src/main/java/com/Project/BoardService/config/security/JwtTokenProvider.java
@@ -0,0 +1,58 @@
+package com.Project.BoardService.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    //토큰 암호화 비밀 키 (64글자 이상)
+    private String secretKey = "asdfsadfqwerwqrweqasfsadfqwerqwerzxvxzvxcqwerqwsdfsxczvzxcfdsafwerwq";
+
+
+    //토큰 유효시간 (30분)
+    private final long validTime = 1000L*30*60;
+
+    //비밀 키 인코딩 -> Base46 이용
+    @PostConstruct
+    private void secretKeyEncoding(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    //JWT 토큰 생성
+    public LogInResponse createToken(Long id){
+
+        Date now = new Date();
+
+        //JWT payload 단위
+        Claims claims = Jwts.claims();
+        claims.put("userId", id);
+
+        //JWT 토큰 생성
+        String accessToken = Jwts.builder()
+                .setClaims(claims)                                         //payload
+                .setIssuedAt(now)                                          //토큰 발행 시간
+                .setExpiration(new Date(now.getTime() + validTime))       //인증 만료 시간
+                .signWith(SignatureAlgorithm.HS256, secretKey)            //암호화 알고리즘 & secretKey
+                .compact();
+
+        //인증 만료 시간 갱신 토큰
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now.getTime() + validTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+
+
+        return LogInResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+}
