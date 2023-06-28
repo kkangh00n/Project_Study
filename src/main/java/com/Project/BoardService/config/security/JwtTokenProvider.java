b53141d1365b498b5cc0fc477adf8dd18c1a082a
kkangh00n <kig2454@gmail.com>
2023-06-28 14:46:53 +0900

    feat: JWT 토큰 복호화

--- src/main/java/com/Project/BoardService/config/security/JwtTokenProvider.java
+++ src/main/java/com/Project/BoardService/config/security/JwtTokenProvider.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,16 +17,14 @@ public class JwtTokenProvider {
     //토큰 암호화 비밀 키 (64글자 이상)
     @Value("${jwt.secretKey}")
     private String secretKey;
-
-
     //토큰 유효시간 (30분)
     private final long validTime = 1000L*30*60;
-
     //비밀 키 인코딩 -> Base46 이용
     @PostConstruct
     private void secretKeyEncoding(){
         secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
     }
+
 
     //JWT 토큰 생성
     public LogInResponse createToken(Long id){
@@ -47,6 +46,21 @@ public class JwtTokenProvider {
         return LogInResponse.builder()
                 .accessToken(accessToken)
                 .build();
+    }
+
+    //JWT 토큰 복호화
+    public Long getMemberId(HttpServletRequest request){
+
+        //요청에서 토큰 GET
+        String accessToken = request.getHeader("Token");
+
+        //토큰 payload GET
+        Claims claims = Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(accessToken)
+                .getBody();
+
+        return claims.get("userId", Long.class);
     }
 
 }
