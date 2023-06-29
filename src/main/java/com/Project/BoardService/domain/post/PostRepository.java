221a66f33336e166895b57d5732fb20ce8d6f1ed
kkangh00n <kig2454@gmail.com>
2023-06-29 16:16:28 +0900

    feat: 특정 게시글 조회 성능 최적화

--- src/main/java/com/Project/BoardService/domain/post/PostRepository.java
+++ src/main/java/com/Project/BoardService/domain/post/PostRepository.java
@@ -1,14 +1,20 @@
 package com.Project.BoardService.domain.post;
 
 import com.Project.BoardService.domain.post.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     //게시글 전체 조회 기능
     List<Post> findTop100AllByOrderByCreateDateDesc();
+
+    @Override
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Post> findById(Long id);
 
     //게시글 검색 기능
     List<Post> findTop100ByTitleContainingOrderByCreateDateDesc(String title);
