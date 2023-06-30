fd400954e715bc388f1d4b3a8560140b656b3380
kkangh00n <kig2454@gmail.com>
2023-06-30 15:37:04 +0900

    refactor: comment 객체 수정

--- src/main/java/com/Project/BoardService/domain/comment/Comment.java
+++ src/main/java/com/Project/BoardService/domain/comment/Comment.java
@@ -1,16 +1,22 @@
 package com.Project.BoardService.domain.comment;
 
+import com.Project.BoardService.domain.common.BaseTimeEntity;
 import com.Project.BoardService.domain.post.Post;
 import com.Project.BoardService.domain.user.User;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
-public class Comment {
+@Getter
+public class Comment extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
     @Column(name = "comment_id")
     private Long id;
+
+    @Column(name = "comment", nullable = false)
+    private String comment;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
@@ -19,5 +25,4 @@ public class Comment {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
     private Post post;
-
 }
