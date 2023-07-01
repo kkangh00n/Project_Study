bd985c5eca12509f5f5d6fd9a6135515a56e3ad4
kkangh00n <kig2454@gmail.com>
2023-07-01 23:34:38 +0900

    feat: 초기 더미 데이터 수정

--- src/main/java/com/Project/BoardService/InitDB.java
+++ src/main/java/com/Project/BoardService/InitDB.java
@@ -1,5 +1,6 @@
 package com.Project.BoardService;
 
+import com.Project.BoardService.domain.comment.Comment;
 import com.Project.BoardService.domain.post.Post;
 import com.Project.BoardService.domain.user.User;
 import jakarta.annotation.PostConstruct;
@@ -34,13 +35,7 @@ public class InitDB {
                     .build();
             em.persist(user1);
 
-            User user2 = User.builder()
-                    .email("kkang_h00n@google.com")
-                    .password("rlarkdgns222")
-                    .build();
-            em.persist(user2);
-
-            for (int i = 0; i < 25; i++) {
+            for (int i = 0; i < 10; i++) {
                 Post post = Post.builder()
                         .title("제목"+i)
                         .content("내용"+i)
@@ -50,15 +45,58 @@ public class InitDB {
                 Thread.sleep(1);
             }
 
-            for (int i = 25; i < 50; i++) {
-                Post post = Post.builder()
-                        .title("제목"+i)
-                        .content("내용"+i)
-                        .user(user2)
-                        .build();
-                em.persist(post);
-                Thread.sleep(1);
-            }
+            User user2 = User.builder()
+                    .email("zxcv1234@google.com")
+                    .password("rlarkdgns222")
+                    .build();
+            em.persist(user2);
+
+            User user3 = User.builder()
+                    .email("qwer1234@google.com")
+                    .password("rlarkdgns222")
+                    .build();
+            em.persist(user3);
+
+            User user4 = User.builder()
+                    .email("asdf1234@google.com")
+                    .password("rlarkdgns222")
+                    .build();
+            em.persist(user4);
+
+            Post post2 = Post.builder()
+                    .title("깍두기")
+                    .content("ㅎㅎ")
+                    .user(user1)
+                    .build();
+            em.persist(post2);
+
+            Comment comment1 = Comment.builder()
+                    .comment("댓글"+1)
+                    .user(user1)
+                    .post(post2)
+                    .build();
+            em.persist(comment1);
+
+            Comment comment2 = Comment.builder()
+                    .comment("댓글"+2)
+                    .user(user2)
+                    .post(post2)
+                    .build();
+            em.persist(comment2);
+
+            Comment comment3 = Comment.builder()
+                    .comment("댓글"+3)
+                    .user(user3)
+                    .post(post2)
+                    .build();
+            em.persist(comment3);
+
+            Comment comment4 = Comment.builder()
+                    .comment("댓글"+4)
+                    .user(user4)
+                    .post(post2)
+                    .build();
+            em.persist(comment4);
 
         }
 
